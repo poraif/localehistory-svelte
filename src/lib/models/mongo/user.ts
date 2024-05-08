b449871c1c3177b22d00a52572f1e3e1bd05b655
poraif <peadar.oraif@gmail.com>
2024-05-08 18:32:24 +0200

    hooks.server file added

---
+++ src/lib/models/mongo/user.ts
@@ -0,0 +1,13 @@
+import type { User } from "$lib/types/placemark-types";
+import { Schema, model } from "mongoose";
+import pkg from "mongoose";
+const { models } = pkg;
+
+const userSchema = new Schema<User>({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String
+});
+
+export const UserMongoose = models["User"] || model("User", userSchema);
